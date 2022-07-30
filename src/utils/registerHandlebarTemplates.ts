9e66496edad022ff5cdd1faac5d179c9814d233d
John Gozde <john@gozde.ca>
2022-07-29 21:37:40 -0600

    Tweak hooks

--- src/utils/registerHandlebarTemplates.ts
+++ src/utils/registerHandlebarTemplates.ts
@@ -23,6 +23,7 @@ import partialIsNullable from '../templates/partials/isNullable.hbs';
 import partialIsReadOnly from '../templates/partials/isReadOnly.hbs';
 import partialIsRequired from '../templates/partials/isRequired.hbs';
 import partialParameters from '../templates/partials/parameters.hbs';
+import partialParametersType from '../templates/partials/parametersType.hbs';
 import partialResult from '../templates/partials/result.hbs';
 import partialSchema from '../templates/partials/schema.hbs';
 import partialSchemaArray from '../templates/partials/schemaArray.hbs';
@@ -101,6 +102,7 @@ export const registerHandlebarTemplates = (): Templates => {
     Handlebars.registerPartial('isReadOnly', Handlebars.template(partialIsReadOnly));
     Handlebars.registerPartial('isRequired', Handlebars.template(partialIsRequired));
     Handlebars.registerPartial('parameters', Handlebars.template(partialParameters));
+    Handlebars.registerPartial('parametersType', Handlebars.template(partialParametersType));
     Handlebars.registerPartial('result', Handlebars.template(partialResult));
     Handlebars.registerPartial('schema', Handlebars.template(partialSchema));
     Handlebars.registerPartial('schemaArray', Handlebars.template(partialSchemaArray));
